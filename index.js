9e80b90ea052e85ed75ccf7983ef919301ac5dbf
Jeroen Coumans <jeroencoumans@gmail.com>
2014-04-27 16:42:50 +0200

    Add ScrollBlocker component

---
+++ index.js
@@ -0,0 +1,5 @@
+var ScrollBlocker = require('src/ScrollBlocker');
+
+module.exports = {
+  ScrollBlocker: ScrollBlocker
+}
---
+++ src/ScrollBlocker.js
@@ -0,0 +1,26 @@
+/** @jsx React.DOM */
+var React = require('react');
+var cx = require('react/lib/cx');
+
+var ScrollBlocker = React.createClass({
+
+  getDefaultProps: function () {
+    return {
+      active: false
+    };
+  },
+
+  render: function () {
+    var classes = cx({
+      'ScrollBlocker': this.props.active
+    });
+
+    return (
+      <div className={cx(classes, this.props.className)}>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+module.exports = ScrollBlocker;
