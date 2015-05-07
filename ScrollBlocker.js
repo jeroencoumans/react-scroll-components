4485c653b4ffbda1f544e99b5653c46b5f8c55a2
Jeroen Coumans <jeroencoumans@gmail.com>
2015-05-07 17:51:26 +0200

    Enable harmony. Fixes #4

--- ScrollBlocker.js
+++ ScrollBlocker.js
@@ -21,7 +21,7 @@ var ScrollBlocker = React.createClass({displayName: "ScrollBlocker",
   },
 
   render: function () {
-    var { style, active, ...other } = this.props;
+    var $__0=      this.props,style=$__0.style,active=$__0.active,other=(function(source, exclusion) {var rest = {};var hasOwn = Object.prototype.hasOwnProperty;if (source == null) {throw new TypeError();}for (var key in source) {if (hasOwn.call(source, key) && !hasOwn.call(exclusion, key)) {rest[key] = source[key];}}return rest;})($__0,{style:1,active:1});
 
     return (
       React.createElement("div", React.__spread({},  other, {style: assign({}, style, active && blockerStyle)}), 
